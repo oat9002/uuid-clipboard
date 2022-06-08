aa3197c2d7c2c2de36cfee07001c8ca25be37751
oat9002 <oat9002@hotmail.com>
2022-06-09 00:30:11 +0700

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/oat9002/uuid-clipboard
+
+go 1.18
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/nsf/termbox-go v1.1.1
+	golang.design/x/clipboard v0.6.2
+)
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+)
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	term "github.com/nsf/termbox-go"
+	"golang.design/x/clipboard"
+)
+
+func main() {
+	err := term.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer term.Close()
+
+	err = clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	isExit := false
+
+	for {
+		uuid := uuid.New().String()
+		clipboard.Write(clipboard.FmtText, []byte(uuid))
+		fmt.Printf("Here: %s - auto save to clipboard\n", uuid)
+		fmt.Println("Press Enter to generate new uuid")
+		fmt.Println("Press any button to exit")
+
+		switch ev := term.PollEvent(); ev.Type {
+		case term.EventKey:
+			switch ev.Key {
+			case term.KeyEnter:
+				term.Sync()
+				isExit = false
+
+			default:
+				term.Sync()
+				isExit = true
+			}
+		case term.EventError:
+			panic(ev.Err)
+		}
+
+		if isExit {
+			break
+		}
+	}
+}
